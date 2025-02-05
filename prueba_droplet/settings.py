ae2837c6f2367b5197907d309a93784a6c164750
Raymondacampo <raymondacamposandoval@gmail.com>
2025-02-05 17:44:16 -0400

    STATIC ROOT ADDED

--- prueba_droplet/settings.py
+++ prueba_droplet/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'prueba_app', 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Keep this if you have additional static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
